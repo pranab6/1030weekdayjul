02d9b32a0086ae733fd84b9589c0ad9a8e9d4cb0
Pranab Sahoo <pranab.sahoo56@gmail.com>
2020-07-23 17:37:10 +0000

    python file created

---
+++ pyt.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+class Test:
+    print("Testing file")
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def add(self):
+        self.c=a+b
+        print("Sum number is",self.c)
+
+a=Test(125,225)
+
